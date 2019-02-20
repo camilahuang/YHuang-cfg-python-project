dbbe31bddd592c26d652bf22b414819abb2b45de
Camilla <604937889@qq.com>
2019-02-20 19:26:24 +0000

    Add hello
    My first repository commit.

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello, world")
+print(2*4)
+
+print('Bob'*3)
+print('Bob'*3)
+print('hello'.upper())
+print('GOODBYE'.lower())
+print("thelord of therings".title())
+
+age=25
+hobby="swimming"
+description="Her age is {} and she likes {}".format(age,hobby)
+print(description)
+
+exmp="The quick brown fox jumps over the lazy dog."
+print(exmp.lower().count('the'))
